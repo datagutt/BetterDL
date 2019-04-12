5eaa2ca175b2b332cc8875ed696e10b828c7ff6a
Thomas Lekanger <datagutt@lekanger.no>
2019-04-12 18:42:24 +0200

    Basic framework in place

--- content.js
+++ content.js
@@ -1,5 +1,15 @@
-// inject the hook
-const script = document.createElement('script')
+// Create the injection script element
+const script = document.createElement('script');
 script.setAttribute('src', chrome.runtime.getURL('inject.js'));
-document.documentElement.appendChild(script)
-script.parentNode.removeChild(script)
+script.defer = 'defer';
+script.async = 'async';
+// We want to be able to use static imports, so set module as type
+script.type = 'module';
+
+// The idea of this is to run the script after app.js is loaded, but before the page-specific scripts are loaded
+let appScript = document.body.querySelector('script[src^="/js/app"]');
+if(appScript){
+    appScript.parentNode.insertBefore(script, appScript.nextSibling);
+}else{
+    document.head.appendChild(script)
+}
---
+++ modules/ChatModule.js
@@ -0,0 +1,79 @@
+// Twitch + bttv + ffz emotes
+import emotes from '../emotes.js';
+console.log('emotes', emotes);
+
+export default class ChatModule {
+    start(){
+        const messages = document.querySelector('.v-chatroom-message .chatbody');
+        let chatMagicDone =
+            messages &&
+            messages.getAttribute('data-betterdl') === "true";
+        // Get current messages
+        if(!chatMagicDone){
+            console.log('BetterDL: Doing chat magic')
+            messages.setAttribute('data-betterdl', true);
+            chatMagicDone = true;
+
+            // Observe new chat messages
+            let observer = new MutationObserver(this.mutator);
+
+            observer.observe(messages, {
+                childList: true,
+                attributes: false,
+                characterData: false,
+                subtree: true
+            });
+
+            // Preloaded messages
+            Array.from(messages.children).forEach((node) => {
+                if(node && node.className == 'position-relative margint-3') {
+                    this.onChatMessage(node.children[0]);
+                }
+            });
+        }else{
+            console.log('BetterDL: Chat magic already done')
+        }
+    }
+    onChatMessage = entry => {
+        // Get the actual text of the message
+        let textNode = entry.querySelector('.chatrow-inner .text');
+        // If this is not a text node
+        if(!textNode || !textNode.innerText) return;
+        // Get each word
+        const rawWords = textNode && textNode.innerText.split(' ');
+        let parsedText = '';
+
+        // Go through each word
+        for(let i = 0, length = rawWords.length; i < length; i++){
+            const word = rawWords[i];
+            // Look for word in emote array
+            const emote = emotes[word];
+
+            if(typeof emote === 'undefined'){
+                parsedText += word + ' ';
+            }else{
+                parsedText += `<img
+                    src="${emote.url}"
+                    title="${word} from ${emote.provider}"
+                    class="emote-img margint-3 clickable"
+                />` + ' ';
+            }
+        }
+        if(parsedText) textNode.innerHTML = parsedText;
+    }
+    mutator = mutations => {
+        return mutations.forEach(mutation => {
+            const { addedNodes, removedNodes } = mutation;
+
+            // Added nodes
+            if(typeof addedNodes !== 'undefined' && addedNodes.length > 0) {
+                for(let i = 0, length = addedNodes.length - 1; i <= length; i++) {
+                    let node = addedNodes[i];
+                    if(node.className == 'position-relative margint-3') {
+                        this.onChatMessage(node.children[0]);
+                    }
+                }
+            }
+        });
+    }
+}
